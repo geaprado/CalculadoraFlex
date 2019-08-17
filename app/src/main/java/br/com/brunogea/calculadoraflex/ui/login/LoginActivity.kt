401f302b888061e861e4a67ff76238b8b34c3d73
Logon Aluno <logonaluno@local.com>
2019-08-17 11:02:36 -0300

    Initial commit

---
+++ app/src/main/java/br/com/brunogea/calculadoraflex/ui/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package br.com.brunogea.calculadoraflex.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.brunogea.calculadoraflex.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
